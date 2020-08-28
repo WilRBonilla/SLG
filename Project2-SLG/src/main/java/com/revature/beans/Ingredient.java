9bd2c77c8f15f378e590688c21de8b401646138b
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-08-28 14:43:51 -0500

    Beans

--- Project2-SLG/src/main/java/com/revature/beans/Ingredient.java
+++ Project2-SLG/src/main/java/com/revature/beans/Ingredient.java
@@ -1,12 +1,14 @@
 package com.revature.beans;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Ingredient {
 
 	@Id
+	@GeneratedValue
 	private int ing_id;
 	private String name;
 	private String units;
