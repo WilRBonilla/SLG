7e604b190496a3ba5068db82c2fc0fe7959d0cba
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-09-01 18:56:46 -0500

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# SLG
-ShoppingListGenerator
--- SLG-NG/src/app/models/Note.ts
+++
@@ -1,9 +0,0 @@
-export class Note {
-    n_id: number;
-    add_notes: string;
-
-    constructor(n_id: number, add_notes: string) {
-        this.n_id = n_id;
-        this.add_notes = add_notes;
-    }
-}
