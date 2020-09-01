50fa7f26dcfc2aac1cba50c328bd2721cdf244a5
Alex Jones <alexander.jones@revature.net>
2020-09-01 13:30:15 -0600

    navbarupdate

--- SLG-NG/src/app/app-routing.module.ts
+++ SLG-NG/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { StockComponent } from './components/stock/stock.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { ShoppinglistComponent } from './components/shoppinglist/shoppinglist.component';
@@ -9,14 +7,11 @@ import { HomeComponent } from './components/home/home.component';
 import { PantryComponent} from './components/pantry/pantry.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
   {path: 'stock', component: StockComponent},
   {path: 'recipe', component: RecipeComponent},
   {path: 'shoppinglist', component: ShoppinglistComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'pantry', component: PantryComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
+  {path: 'pantry', component: PantryComponent}
 ];
 
 @NgModule({
