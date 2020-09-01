385b46deb8a4ea60a949e93c40876514c7b7082a
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-09-01 17:50:09 -0500

    shoppinglistwork

--- SLG-NG/src/app/components/shoppinglist/shoppinglist.component.ts
+++ SLG-NG/src/app/components/shoppinglist/shoppinglist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListEntry } from 'src/app/models/ShoppingListEntry';
+import { Ingredient } from 'src/app/models/Ingredient';
+import { Shopper } from 'src/app/models/Shopper';
+import { Note } from 'src/app/models/Note';
 
 @Component({
   selector: 'app-shoppinglist',
@@ -12,4 +16,16 @@ export class ShoppinglistComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ing: Ingredient = new Ingredient(1, "Chicken Breasts", "number");
+  user: Shopper = new Shopper("william", "bonilla", "username3", "password");
+  note: Note = new Note(1, "TEST NOTES");
+  sl: ShoppingListEntry = new ShoppingListEntry(9001,this.ing, this.user, 2,  this.note);
+  shoppingList: Array<ShoppingListEntry> = [
+    this.sl
+
+  ]
+
+
+
+  
 }
