8acd3c44c650cbf7435e148f264d1b6ffe41416d
Jack McLean <mcleanojack2@gmail.com>
2020-09-02 06:28:52 -0400

    Cleaned up Tests @Mock

--- Project2-SLG/src/test/java/com/revature/demo/Project2SlgApplicationTests.java
+++ Project2-SLG/src/test/java/com/revature/demo/Project2SlgApplicationTests.java
@@ -1,12 +1,13 @@
 package com.revature.demo;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,17 +16,23 @@ import com.revature.beans.Ingredient;
 import com.revature.controllers.IngredientController;
 import com.revature.repositories.IngredientRepository;
 import com.revature.services.IngredientService;
+import com.revature.services.IngredientServiceImpl;
 
 @SpringBootTest
 class Project2SlgApplicationTests {
-	@Autowired 
-	IngredientService is;
+	@Mock
+	IngredientRepository ir;
+	@InjectMocks
+	IngredientServiceImpl is;
 
+	Ingredient i = new Ingredient(500,"test","test");
 	@Autowired 
 	IngredientRepository irp;
-	
 	@Autowired
 	IngredientController ic;
+	@Autowired
+	IngredientService isa;
+	
 	
 	@Test
 	void contextLoads() {
@@ -33,62 +40,41 @@ class Project2SlgApplicationTests {
 	
 	@Test
 	void addIngredient() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
 		Mockito.when(ir.save(i)).thenReturn(i);
 		Ingredient t = is.addIngredient(i);
-		System.out.println(t);
-		t.setIng_id(500);
 		Assertions.assertEquals(i, t);
-
 	}
 	
 	@Test
 	void addIngredientController() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
 		Mockito.when(ir.save(i)).thenReturn(i);
 		Ingredient t = ic.addIngredient(i);
-		t.setIng_id(500);
 		Assertions.assertEquals(i, t);
 	}
+
 	@Test
-	void getIngredient() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
+	void getIngredientByName() {
 		Mockito.when(ir.findByName(i.getName())).thenReturn(i);
 		Ingredient t = is.findByName(i.getName());
-		i.setIng_id(t.getIng_id());
 		Assertions.assertEquals(i, t);
 	}
+	
 	@Test
-	void getIngredientController() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
+	void getIngredientNameController() {
 		Mockito.when(ir.findByName(i.getName())).thenReturn(i);
 		Ingredient t = ic.findByName(i.getName());
-		i.setIng_id(t.getIng_id());
 		Assertions.assertEquals(i, t);
 	}
-	@Test
-	void FindIngredientByID() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
-		Mockito.when(ir.findById(i.getIng_id())).thenReturn(Optional.of(i));
-		Ingredient t = is.getIngredient(i.getIng_id());
-		Assertions.assertEquals(i, t);
-	}
+
 	@Test
 	void FindIngredientByIDController() {
-		Ingredient i = new Ingredient(500,"test","test");
-		IngredientRepository ir = Mockito.mock(IngredientRepository.class);
 		Mockito.when(ir.findById(i.getIng_id())).thenReturn(Optional.of(i));
 		Ingredient t = ic.getIngredient(i.getIng_id());
 		Assertions.assertEquals(i, t);
 	}
 	@Test
 	void GetAllIngredients() {
-		Assertions.assertEquals(irp.findAll(), is.getAllIngredients());		
+		Assertions.assertEquals(irp.findAll(), isa.getAllIngredients());		
 	}
 	@Test
 	void GetAllIngredientsController() {
@@ -99,11 +85,5 @@ class Project2SlgApplicationTests {
 		Ingredient i = new Ingredient(500,"test","test");
 		assertTrue(ic.deleteIngredient(500));
 	}
-	@Test
-	void DeleteIngredientFalse() {
-		Ingredient i = new Ingredient(500,"test","test");
-		assertFalse(ic.deleteIngredient(800));
-	}
-	
 
 }
