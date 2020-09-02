0ec8c34ad49f39ae04d462d15a2618ac9417b69f
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-09-02 16:18:27 -0500

    asdf

---
+++ SLG-NG/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from 'src/app/services/navbar.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(public nav:NavbarService) { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
