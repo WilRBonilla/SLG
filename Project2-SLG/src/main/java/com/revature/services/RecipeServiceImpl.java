1bbc2c2cd14daa9b49a3ac42f2fa290cc38750f1
Jack McLean <mcleanojack2@gmail.com>
2020-09-03 08:52:00 -0400

    Updated backend for Recipe Search

--- Project2-SLG/src/main/java/com/revature/services/RecipeServiceImpl.java
+++ Project2-SLG/src/main/java/com/revature/services/RecipeServiceImpl.java
@@ -46,54 +46,90 @@ public class RecipeServiceImpl implements RecipeService {
 
 	@Override
 	public List<Recipe> findByCuisine(String cuisine) {
-		return rr.findAllByCuisine(cuisine);
-	}
-
-	@Override
-	public List<Recipe> findByTag1(String tag1) {
-		List<Recipe> lr1 =rr.findAllByTag1(tag1);
-		List<Recipe> lr2 =rr.findAllByTag2(tag1);
+		List<Recipe> lr1 =rr.findAllByTag1(cuisine);
+		List<Recipe> lr2 =rr.findAllByTag2(cuisine);
+		List<Recipe> lr3 =rr.findAllByCuisine(cuisine);
 		lr1.addAll(lr2);
+		lr1.addAll(lr3);
 		return lr1;
 	}
 
-	@Override
-	public List<Recipe> findByTag2(String tag2) {
-		List<Recipe> lr1 =rr.findAllByTag1(tag2);
-		List<Recipe> lr2 =rr.findAllByTag2(tag2);
-		lr1.addAll(lr2);
-		return lr1;
-	}
-
-	@Override
-	public List<Recipe> findByTag1AndTag2(String tag1, String tag2) {
-		List<Recipe> lr1 =rr.findAllByTag1AndTag2(tag1, tag2);
-		List<Recipe> lr2 =rr.findAllByTag1AndTag2(tag2, tag1);
-		lr1.addAll(lr2);
-		return lr1;
-	}
 
 	@Override
 	public List<Recipe> findByCuisineAndTag1(String cuisine, String tag1) {
 		List<Recipe> lr1 =rr.findAllByCuisineAndTag1(cuisine, tag1);
-		List<Recipe> lr2 =rr.findAllByCuisineAndTag2(cuisine, tag1);
+		List<Recipe> lr2 =rr.findAllByCuisineAndTag1(tag1, cuisine);
+		List<Recipe> lr3 =rr.findAllByCuisineAndTag2(cuisine, tag1);
+		List<Recipe> lr4 =rr.findAllByCuisineAndTag2(tag1,cuisine);
+		List<Recipe> lr5 =rr.findAllByTag1AndTag2(cuisine, tag1);
+		List<Recipe> lr6 =rr.findAllByTag1AndTag2(tag1,cuisine);
 		lr1.addAll(lr2);
+		lr1.addAll(lr3);
+		lr1.addAll(lr4);
+		lr1.addAll(lr5);
+		lr1.addAll(lr6);
 		return lr1;
 	}
 
-	@Override
-	public List<Recipe> findByCuisineAndTag2(String cuisine, String tag2) {
-		List<Recipe> lr1 =rr.findAllByCuisineAndTag1(cuisine, tag2);
-		List<Recipe> lr2 =rr.findAllByCuisineAndTag2(cuisine, tag2);
-		lr1.addAll(lr2);
-		return lr1;
-	}
 
 	@Override
 	public List<Recipe> findByCuisineAndTag1AndTag2(String cuisine, String tag1, String tag2) {
 		List<Recipe> lr1 =rr.findAllByCuisineAndTag1AndTag2(cuisine,tag1, tag2);
 		List<Recipe> lr2 =rr.findAllByCuisineAndTag1AndTag2(cuisine,tag2, tag1);
+		List<Recipe> lr3 =rr.findAllByCuisineAndTag1AndTag2(tag2, tag1,cuisine);
+		List<Recipe> lr4 =rr.findAllByCuisineAndTag1AndTag2(tag2,cuisine, tag1);
+		List<Recipe> lr5 =rr.findAllByCuisineAndTag1AndTag2(tag1,cuisine,tag2);
+		List<Recipe> lr6 =rr.findAllByCuisineAndTag1AndTag2(tag1,tag2, cuisine);
 		lr1.addAll(lr2);
+		lr1.addAll(lr3);
+		lr1.addAll(lr4);
+		lr1.addAll(lr5);
+		lr1.addAll(lr6);
 		return lr1;
 	}
 }
+
+
+//	Pretty sure these are not needed anymore but leaving them for now just in case
+//		Maybe we'll end up needing some of it for Admin functionality
+
+
+//@Override
+//public List<Recipe> findByTag1(String tag1) {
+//	List<Recipe> lr1 =rr.findAllByTag1(tag1);
+//	List<Recipe> lr2 =rr.findAllByTag2(tag1);
+//	List<Recipe> lr3 =rr.findAllByCuisine(tag1);
+//	lr1.addAll(lr2);
+//	lr1.addAll(lr3);
+//	return lr1;
+//}
+//
+//@Override
+//public List<Recipe> findByTag2(String tag2) {
+//	List<Recipe> lr1 =rr.findAllByTag1(tag2);
+//	List<Recipe> lr2 =rr.findAllByTag2(tag2);
+//	List<Recipe> lr3 =rr.findAllByCuisine(tag2);
+//	lr1.addAll(lr2);
+//	lr1.addAll(lr3);
+//	return lr1;
+//}
+//
+//
+//@Override
+//public List<Recipe> findByTag1AndTag2(String tag1, String tag2) {
+//	List<Recipe> lr1 =rr.findAllByTag1AndTag2(tag1, tag2);
+//	List<Recipe> lr2 =rr.findAllByTag1AndTag2(tag2, tag1);
+//	List<Recipe> lr3 =rr.findAllByCuisineAndTag1(tag2, tag1);
+//	List<Recipe> lr4 =rr.findAllByCuisineAndTag2(tag1, tag2);
+//	lr1.addAll(lr2);
+//	lr1.addAll(lr3);
+//	lr1.addAll(lr4);
+//	return lr1;
+//}
+//@Override
+//public List<Recipe> findByCuisineAndTag2(String cuisine, String tag2) {
+//	List<Recipe> lr1 =rr.findAllByCuisineAndTag1(cuisine, tag2);
+//	List<Recipe> lr2 =rr.findAllByCuisineAndTag2(cuisine, tag2);
+//	lr1.addAll(lr2);
+//	return lr1;
+//} 
