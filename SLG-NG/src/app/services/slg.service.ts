71084600a53a8520292799e699718ed29e52cc92
Kevin Malazarte <kevin.malazarte@gmail.com>
2020-09-02 20:11:33 -0400

    reorganized the service.ts file

--- SLG-NG/src/app/services/slg.service.ts
+++ SLG-NG/src/app/services/slg.service.ts
@@ -15,11 +15,44 @@ import { Stock } from '../models/Stock';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SlgService {
 
   constructor(private http: HttpClient) { }
 
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  getRecipeResults(search: string): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>('http://localhost:8080/recipe/search' + search);
+  }
+
+  getOneShoppingListEntry(eid: number): Observable<ShoppingListEntry> {
+    return this.http.get<ShoppingListEntry>('http://localhost:8080/getShoppingListEntry/' + eid);
+  }
+
+  addToShoppingList(shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]> {
+    // Returns only the updated entities that we can add to our current array.
+    return this.http.post<ShoppingListEntry[]>('http://localhost:8080/addShoppingList', shopList, { headers: this.headers });
+  }
+
+  updateShoppingList(uid: number, shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]> {
+    // Returns only the updated entities in the shopping list.
+    return this.http.put<ShoppingListEntry[]>('http://localhost:8080/shoppingList/' + uid, shopList, { headers: this.headers })
+  }
+
+  deleteShoppingListEntry(eid: number) {
+    // Returns nothing of importance, but have to see if we can still measure success.
+    return this.http.delete('http://localhost:8080/shoppingListEntry/' + eid)
+  }
+
+  getUserShoppingListEntries(uid: number): Observable<ShoppingListEntry[]> {
+    // Returns an array of ShoppingListEntry objects.
+    return this.http.get<ShoppingListEntry[]>('http://localhost:8080/shoppingList/' + uid);
+  }
+
+  addRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>('http://localhost:8080/recipe', recipe, { headers: this.headers });
+  }
 
   getLoginInfo(shopper: Shopper): Observable<Shopper> {
     return this.http.post<Shopper>('http://localhost:8080/login', shopper, {headers: this.headers});
@@ -36,6 +69,14 @@ export class SlgService {
 
   getAllIngredients(): Observable<Ingredient[]> {
     return this.http.get<Ingredient[]>('http://localhost:8080/ingredient');
+  }
+
+  addIngredient(ingredient: Ingredient): Observable<Ingredient> {
+    return this.http.post<Ingredient>('http://localhost:8080/ingredient', ingredient, { headers: this.headers });
+  }
+
+  deleteIngredient(ingId: number): Observable<Ingredient> {
+    return this.http.delete<Ingredient>('http://localhost:8080/ingredient/' + ingId)
   }
 
 
@@ -58,63 +99,43 @@ export class SlgService {
 
   addPantryList(pantryList: Pantry[]): Observable<Pantry[]> {
     return this.http.post<Pantry[]>('http://localhost:8080/pantry', pantryList, {headers: this.headers});
-
-  addIngredient(ingredient :Ingredient): Observable<Ingredient> {
-    return this.http.post<Ingredient>('http://localhost:8080/ingredient', ingredient, {headers: this.headers});
-
-  }
-
-  deleteIngredient(ingId: number): Observable<Ingredient>{
-    return this.http.delete<Ingredient>('http://localhost:8080/ingredient/' + ingId)
   }
 
   // -------------------------SHOPPING LIST ENTRY SERVICES ------------------------------------------------------
   // This function gets the shopping list by userid.
-  getUserShoppingListEntries(uid: number): Observable<ShoppingListEntry[]>{
-    // Returns an array of ShoppingListEntry objects.
-    return this.http.get<ShoppingListEntry[]>('http://localhost:8080/shoppingList/'+ uid);
-  }
+  // getUserShoppingListEntries(uid: number): Observable<ShoppingListEntry[]>{
+  //   // Returns an array of ShoppingListEntry objects.
+  //   return this.http.get<ShoppingListEntry[]>('http://localhost:8080/shoppingList/'+ uid);
+  // }
 
   // This function retrieves a single entry. Doubt it'll be useful. here just in case
-  getOneShoppingListEntry(eid: number): Observable<ShoppingListEntry>{
-    return this.http.get<ShoppingListEntry>('http://localhost:8080/getShoppingListEntry/'+eid);
-  }
+  // getOneShoppingListEntry(eid: number): Observable<ShoppingListEntry>{
+  //   return this.http.get<ShoppingListEntry>('http://localhost:8080/getShoppingListEntry/'+eid);
+  // }
 
   // This function adds an array of (one or more) entries to the DB
-  addToShoppingList(shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]>{
-    // Returns only the updated entities that we can add to our current array.
-    return this.http.post<ShoppingListEntry[]>('http://localhost:8080/addShoppingList', shopList, {headers: this.headers});
-  }
+  // addToShoppingList(shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]>{
+  //   // Returns only the updated entities that we can add to our current array.
+  //   return this.http.post<ShoppingListEntry[]>('http://localhost:8080/addShoppingList', shopList, {headers: this.headers});
+  // }
 
   // This function appends UID to url and adds (or updates current entries) to the DB.
-  updateShoppingList(uid:number, shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]>{
-    // Returns only the updated entities in the shopping list.
-    return this.http.put<ShoppingListEntry[]>('http://localhost:8080/shoppingList/' + uid, shopList, {headers: this.headers})
-  }
+  // updateShoppingList(uid:number, shopList: ShoppingListEntry[]): Observable<ShoppingListEntry[]>{
+  //   // Returns only the updated entities in the shopping list.
+  //   return this.http.put<ShoppingListEntry[]>('http://localhost:8080/shoppingList/' + uid, shopList, {headers: this.headers})
+  // }
 
   // This function deletes a SLE with an eid.
-  deleteShoppingListEntry(eid: number){
-    // Returns nothing of importance, but have to see if we can still measure success.
-    return this.http.delete('http://localhost:8080/shoppingListEntry/' + eid)
-  }
+  // deleteShoppingListEntry(eid: number){
+  //   // Returns nothing of importance, but have to see if we can still measure success.
+  //   return this.http.delete('http://localhost:8080/shoppingListEntry/' + eid)
+  // }
 
   // -----------------------------END SHOPPING LIST ENTRY SERVICES-----------------------------------------------------
 
   // -----------------------------SHOPPER SERVICES --------------------------------------------------------------------
 
   // -----------------------------END SHOPPER SERVICES ----------------------------------------------------------------
-
-
-  // -----------------------------RECIPE SERVICES ---------------------------------------------------------------------
-
-  getRecipeResults(search: string): Observable<Recipe[]>{
-    return this.http.get<Recipe[]>('http://localhost:8080/recipe/search'+ search);
-  }
-
-  addRecipe(recipe: Recipe): Observable<Recipe> {
-    return this.http.post<Recipe>('http://localhost:8080/recipe', recipe, { headers: this.headers });
-  }
-
 
   // -----------------------------END RECIPE SERVICES -----------------------------------------------------------------
 }
