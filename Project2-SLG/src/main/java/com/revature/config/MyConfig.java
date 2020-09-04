a3dfe9aa8a410647e4967395fdc044637cb500e6
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-09-04 16:20:00 -0500

    Update MyConfig.java

--- Project2-SLG/src/main/java/com/revature/config/MyConfig.java
+++ Project2-SLG/src/main/java/com/revature/config/MyConfig.java
@@ -18,7 +18,7 @@ public class MyConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4200")
+				.allowedOrigins("*")
 				.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
