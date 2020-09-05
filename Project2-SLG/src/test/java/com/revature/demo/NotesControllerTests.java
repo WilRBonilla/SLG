e30a94e8c846dc97fbac5588d6166009432077ad
Will Bonilla <67116391+WilRBonilla@users.noreply.github.com>
2020-09-05 01:03:54 -0500

    Update NotesControllerTests.java

--- Project2-SLG/src/test/java/com/revature/demo/NotesControllerTests.java
+++ Project2-SLG/src/test/java/com/revature/demo/NotesControllerTests.java
@@ -14,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.hamcrest.Matcher;
 import org.hibernate.dialect.Cache71Dialect;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
