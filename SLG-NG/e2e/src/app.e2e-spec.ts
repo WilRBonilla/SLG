4a2ab27a8b7cda285f71e486ffe493f4b23ef394
Alex Jones <alexander.jones@revature.net>
2020-09-07 14:17:32 -0600

    pantry login testing

--- SLG-NG/e2e/src/app.e2e-spec.ts
+++ SLG-NG/e2e/src/app.e2e-spec.ts
@@ -122,6 +122,63 @@ describe('SLG Login Component', () => {
 
   
   });
+
+  it('should add a pantry item', ()=>{
+    browser.get(page);
+    let usernameInput = element(by.id("usernameText"));
+    usernameInput.sendKeys("aj");
+    let passwordInput = element(by.id("passwordText"));
+    passwordInput.sendKeys("12");
+    let loginButton = element(by.id("loginButton"));
+    loginButton.click();
+    let pantryNavButton = element(by.id("pantryNavButton"));
+    pantryNavButton.click();
+    let addFoodItemButton = element(by.id("addFoodItemButton"));
+    addFoodItemButton.click();
+    let selectingredient = element(by.id("selectingredient"));
+    selectingredient.click();
+    let selectOption = element(by.xpath("//*[@id='selectingredient']/option[1]"));
+    selectOption.click();
+    let addinput = element(by.id("addinput"));
+    addinput.sendKeys("15");
+    let addPantryButton = element(by.id("addPantryButton"));
+    addPantryButton.click();
+
+  })
+
+  it('should delete a pantry item', ()=>{
+    browser.get(page);
+    let usernameInput = element(by.id("usernameText"));
+    usernameInput.sendKeys("aj");
+    let passwordInput = element(by.id("passwordText"));
+    passwordInput.sendKeys("12");
+    let loginButton = element(by.id("loginButton"));
+    loginButton.click();
+    let pantryNavButton = element(by.id("pantryNavButton"));
+    pantryNavButton.click();
+    let selectedRow = element(by.xpath("//*[@id='RecipeSeletions']/tr[1]"));
+    selectedRow.click();
+    let removeFoodButton = element(by.id("removeFoodButton"));
+    
+  })
+
+  it('should update a pantry item', ()=>{
+    browser.get(page);
+    let usernameInput = element(by.id("usernameText"));
+    usernameInput.sendKeys("aj");
+    let passwordInput = element(by.id("passwordText"));
+    passwordInput.sendKeys("12");
+    let loginButton = element(by.id("loginButton"));
+    loginButton.click();
+    let pantryNavButton = element(by.id("pantryNavButton"));
+    pantryNavButton.click();
+    let selectedRow = element(by.xpath("//*[@id='RecipeSeletions']/tr[1]"));
+    let addinput = element(by.id("addinput"));
+    addinput.sendKeys("100");
+    let updateFoodItem = element(by.id("updateFoodItem"));
+    updateFoodItem.click();
+    
+  })
   
 
   it('should navigate to recipes', ()=>{
